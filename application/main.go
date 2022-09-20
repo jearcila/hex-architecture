234e65473fd16fd29de2508753a37444c89a6289
Jhon Arcila <jhon.arcila@mercadolibre.com.co>
2022-09-19 22:10:33 -0500

    Update main.go

--- application/main.go
+++ application/main.go
@@ -2,16 +2,8 @@ package main
 
 import (
 	"fmt"
-	"time"
-
-	"github.com/jearcila/hex-architecture/application/integration"
 )
 
 func main() {
-	if err := integration.Run(); err != nil {
-		for {
-			fmt.Println(err)
-			time.Sleep(1 * time.Second)
-		}
-	}
+	fmt.Println("Hola mundo")
 }
